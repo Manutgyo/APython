9182138f7d194adf310481fe364c8794ed45dd5f
Manutgyo <manuela.tamayog@konradlorenz.edu.co>
2023-04-01 12:00:42 -0500

    Primer commit

---
+++ Condicionales.py
@@ -0,0 +1,7 @@
+a = 7
+b = 0
+if a > b and a <= 5:
+    print ("A es mayor que B y menor que 5")
+else:
+    print ("A no cumple las características")
+
---
+++ prueba.py
@@ -0,0 +1,8 @@
+a= 2
+b= 2.1
+c =2+4j
+#print ((type(a)),type(b), type(c))
+
+#Operadores módulo
+var = 21%2
+print(var)
